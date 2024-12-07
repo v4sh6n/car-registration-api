b7afc7ddb369f4cd49ccee5da75f83192faa6b6f
v4sh6n <128129211+v4sh6n@users.noreply.github.com>
2024-12-07 17:12:13 +0100

    Initial commit: Setup Spring Boot Car Registration API

---
+++ car-registry/src/main/java/com/example/car_registry/CarRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.example.car_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRegistryApplication.class, args);
+	}
+
+}
---
+++ car-registry/src/test/java/com/example/car_registry/CarRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.car_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
